a04c0aa0ba93b63cd1e132f06c8a8695e668cb68
clas0429 <39353575+clas0429@users.noreply.github.com>
2025-10-28 14:13:08 +0800

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Guide Magnets v2</title>
+  <title>Guide Magnets</title>
   <link rel="stylesheet" href="assets/css/local-tailwind.css">
   <link rel="stylesheet" href="assets/css/tokens.css">
 </head>
