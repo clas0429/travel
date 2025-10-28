5d945babb32a529477ab434d0464c9390275c4f3
clas0429 <39353575+clas0429@users.noreply.github.com>
2025-10-28 14:12:06 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,6 @@
 <footer class="gm-footer">
   <div class="gm-container gm-footer__inner">
-    <div>Guide Magnets v2 提供即時旅遊導覽與內容管理，所有資料均由專業團隊維護。</div>
-    <div>需要客製化整合或企業授權服務，歡迎 <a href="mailto:hello@guidemagnets.com">聯絡我們</a>。</div>
+    <div>Guide Magnets v2 提供即時旅遊導覽與內容管理，所有資料均由六和高中發明展團隊維護。</div>
   </div>
 </footer>
 </body>
